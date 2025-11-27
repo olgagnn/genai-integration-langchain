625430ac7cb4ccafd98fdc3c0e968c970a45c566
Olga Giannouchou <115019595+olgagnn@users.noreply.github.com>
2025-11-27 20:45:46 +0000

    sef

--- genai-integration-langchain/vector_search.py
+++ genai-integration-langchain/vector_search.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_neo4j import Neo4jGraph
+from langchain_neo4j import Neo4jVector
+from langchain_openai import OpenAIEmbeddings
 
 # Connect to Neo4j
 graph = Neo4jGraph(
@@ -13,9 +15,18 @@ graph = Neo4jGraph(
 )
 
 # Create the embedding model
+embedding_model = OpenAIEmbeddings(model="text-embedding-ada-002")
 
 # Create Vector
+plot_vector = Neo4jVector.from_existing_index(
+    embedding_model,
+    graph=graph,
+    index_name="moviePlots",
+    embedding_node_property="plotEmbedding",
+    text_node_property="plot",
+)
 
 # Search for similar movie plots
-
-# Parse the documents
+plot = "Toys come alive"
+result = plot_vector.similarity_search(plot, k=3)
+print(result)
